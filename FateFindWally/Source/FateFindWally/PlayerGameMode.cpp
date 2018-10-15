6d5b82e1674a688a513a357fa7cae094778e3946
DraynaRue <mattie0@hotmail.co.uk>
2018-10-15 16:34:17 +0100

    Added custom gamemode for player

--- FateFindWally/Source/FateFindWally/PlayerGameMode.cpp
+++ FateFindWally/Source/FateFindWally/PlayerGameMode.cpp
@@ -1,7 +1,17 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PlayerGameMode.h"
+#include "FateFindWallyHUD.h"
+#include "FateFindWallyCharacter.h"
+#include "UObject/ConstructorHelpers.h"
 
+APlayerGameMode::APlayerGameMode()
+	: Super()
+{
+	// set default pawn class
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/PlayerCharacterBP"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
-
-
+	// set fefault HUD
+	HUDClass = AFateFindWallyHUD::StaticClass();
+}
