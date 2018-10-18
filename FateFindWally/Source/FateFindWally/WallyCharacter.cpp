c94563ad2266ad419e02a86af1b8794b75e30227
DraynaRue <mattie0@hotmail.co.uk>
2018-10-18 18:23:01 +0100

    Trying to get player - wally collision to work
    Needs fixing

--- FateFindWally/Source/FateFindWally/WallyCharacter.cpp
+++ FateFindWally/Source/FateFindWally/WallyCharacter.cpp
@@ -1,6 +1,12 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "WallyCharacter.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/PrimitiveComponent.h"
+
+#include "Camera/CameraComponent.h"
+#include "Engine.h"
+
 
 // Sets default values
 AWallyCharacter::AWallyCharacter()
@@ -15,12 +21,42 @@ void AWallyCharacter::BeginPlay()
 {
 	Super::BeginPlay();
 	SetActorLocation(SpawnLocations[FMath::RandRange(0, 4)]->GetActorLocation());
+
+	//GetCapsuleComponent()->BeginComponentOverlap()
+	this->GetCapsuleComponent()->BeginComponentOverlap.AddDynamic(this, &AWallyCharacter::OnHit);
+
+	//this->GetCapsuleComponent()
+	//FOverlapInfo a;
+	//this->GetCapsuleComponent()->BeginComponentOverlap(a, true).AddDynamic(this, &AWallyCharacter::OnHit);
+}
+
+
+//DECLARE_DYNAMIC_MULTICAST_DELEGATE_SixParams(FComponentBeginOverlapSignature, UPrimitiveComponent*, OverlappedComponent, AActor*, OtherActor, UPrimitiveComponent*, OtherComp, int32, OtherBodyIndex, bool, bFromSweep, const FHitResult &, SweepResult);
+
+//FComponentBeginOverlapSignature 
+//UPrimitiveComponent* OverlappedComponent, 
+//AActor*, OtherActor, 
+//UPrimitiveComponent*, OtherComp, 
+//int32, OtherBodyIndex, 
+//bool, bFromSweep, 
+//const FHitResult &, SweepResult
+
+void AWallyCharacter::OnHit(class UPrimitiveComponent* HitComp, 
+	                        class AActor* OtherActor, 
+							class UPrimitiveComponent* OtherComp,
+							int32 OtherBodyIndex, 
+						    bool bFromSweep, 
+						    const FHitResult & SweepResult )
+//(const FOverlapInfo& OtherOverlap, bool bDoNotifies, bool bSkipNotifySelf)
+{
+
 }
 
 // Called every frame
 void AWallyCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
 }
 
 // Called to bind functionality to input
--- FateFindWally/Source/FateFindWally/WallyCharacter.h
+++ FateFindWally/Source/FateFindWally/WallyCharacter.h
@@ -3,8 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
 #include "GameFramework/Character.h"
+
 #include "WallyCharacter.generated.h"
+
 
 UCLASS()
 class FATEFINDWALLY_API AWallyCharacter : public ACharacter
@@ -19,17 +22,28 @@ public:
 	UPROPERTY(Category = Gameplay, EditAnywhere, BlueprintReadWrite)
 		TArray<class AActor*> SpawnLocations;
 	
+	//UFUNCTION(/*custom parameters*/)
+	//	void OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit);
+
+
+	UFUNCTION()
+	void OnHit(class UPrimitiveComponent* HitComp,
+		class AActor* OtherActor,
+		class UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex,
+		bool bFromSweep,
+		const FHitResult & SweepResult);
+	//(const FOverlapInfo& OtherOverlap,bool bDoNotifies, bool bSkipNotifySelf);
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-
+	
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
 	
 };
